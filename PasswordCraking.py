27af8d65ffa2946349d66bfd08e92f3be5077c26
Talha Mahamud <95077791+talhamahamud@users.noreply.github.com>
2021-12-05 07:09:35 +0600

    Add files via upload

---
+++ PasswordCraking.py
@@ -0,0 +1,14 @@
+import random
+
+while True:
+    x = ["1", "2", "3", "4","5","6","7","8","9","0"]
+    rr = random.choice(x)
+    hh = random.choice(x)
+    gg = random.choice(x)
+    tt = random.choice(x)
+    pp = int(rr + hh + gg + tt)
+    print("trying.....")
+    if pp==1324: #here 1324 is the password
+        print("Congratulaion! we have got the password!!! and that is 1324")
+        break
+
